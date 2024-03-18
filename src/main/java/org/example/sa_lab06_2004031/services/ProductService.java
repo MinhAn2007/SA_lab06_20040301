c925abf55a8962c28737ddabafe99437c96cf9de
minhan <voongocminhan20072002@gmail.com>
2024-03-18 17:02:47 +0700

    create MailingService

--- src/main/java/org/example/sa_lab06_2004031/services/ProductService.java
+++ src/main/java/org/example/sa_lab06_2004031/services/ProductService.java
@@ -1,9 +1,12 @@
 package org.example.sa_lab06_2004031.services;
 
+import jakarta.jms.Message;
+import jakarta.jms.TextMessage;
 import org.example.sa_lab06_2004031.models.Product;
 import org.example.sa_lab06_2004031.repositories.ProductOrderRepository;
 import org.example.sa_lab06_2004031.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,5 +18,26 @@ public class ProductService {
     private ProductRepository productRepository;
     public List<Product> findAll() {
         return productRepository.findAll();
+    }
+    @JmsListener(destination = "order_products")
+    public void processOrder(final Message jsonMessage) throws Exception {
+        String messageData = null;
+        String response = null;
+        if(jsonMessage instanceof TextMessage) {
+            //1. read message data
+            String encodeJson = ((TextMessage) jsonMessage).getText();
+            //2. ==> decode
+//            List<Product> products=EncodingUtils.decode(encodeJson);
+            //3. check for quantity
+//            String result = makeOrder(products);
+            //4. make order or reject
+//            MailingService.sendTextMail("@gmail.com",result);
+            //5. send email
+        }
+
+    }
+
+    private String makeOrder(List<Product> products) {
+        return null;
     }
 }
---
+++ src/main/java/org/example/sa_lab06_2004031/utils/CONSTANTS.java
@@ -0,0 +1,8 @@
+package org.example.sa_lab06_2004031.utils;
+
+public interface CONSTANTS {
+    String ORDER_INFORMATION_EMAIL = "vongocminhan20072002@gmail.com";
+    String HOST_MAIL_IDENTITY = "vongocminhan20072002@gmail.com";
+    String HOST_MAIL_SECRET = "ABC123!@#";
+
+}
