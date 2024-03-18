9afd4f930adea549bbb986628f8524503ab85cba
minhan <voongocminhan20072002@gmail.com>
2024-03-18 16:18:28 +0700

    init

---
+++ src/main/java/org/example/sa_lab06_2004031/SaLab062004031Application.java
@@ -0,0 +1,13 @@
+package org.example.sa_lab06_2004031;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaLab062004031Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaLab062004031Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sa_lab06_2004031/SaLab062004031ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sa_lab06_2004031;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaLab062004031ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
